535dbcdaed4f8ca8b59452139d8b057c5ceedb30
ConstantineM <mez365@mail.ru>
2023-09-13 13:33:22 +0300

    DI, IoC, Beans, BeanFactory, ApplicationContext [#505070]

---
+++ README.md
@@ -0,0 +1,14 @@
+
+About project.
+----------
+Studying and applying the core technologies
+of Spring Framework:
+
+1. DI, IoC, Beans, BeanFactory, ApplicationContext.
+2. Spring Bean Life Cycle.
+3. Container configuration.
+4. Bean definitions.
+
+Contacts.
+----------
+- Telegram - https://t.me/MAConsta.
---
+++ src/main/java/ru/job4j_di/ContextByListOfClasses.java
@@ -0,0 +1,32 @@
+package ru.job4j_di;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.job4j_di.model.StartUI;
+import ru.job4j_di.model.Store;
+
+/**
+ * Данный класс описывает создание бина
+ * через ApplicationContext.
+ *
+ * ApplicationContext - это расширенный
+ * интерфейс {@link BeanFactory}.
+ *
+ * 1.Создаем контекст.
+ * 2.Регистрируем в контексте классы,
+ * которые должны стать бинами и обновляем контекст.
+ * 3.Получаем готовый бин и в дальнейшем
+ * его используем.
+ */
+public class ContextByListOfClasses {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Store.class, StartUI.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.print();
+    }
+}
