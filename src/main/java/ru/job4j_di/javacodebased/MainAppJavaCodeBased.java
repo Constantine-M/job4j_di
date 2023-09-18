f2008aa95305922830041beef24cfd3f439e0a20
ConstantineM <mez365@mail.ru>
2023-09-18 21:11:45 +0300

    Bean definitions [#505075]

---
+++ src/main/java/ru/job4j_di/javacodebased/MainAppJavaCodeBased.java
@@ -0,0 +1,12 @@
+package ru.job4j_di.javacodebased;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainAppJavaCodeBased {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        Person person = context.getBean("myPerson", Person.class);
+        person.printNamesPets();
+        context.close();
+    }
+}
